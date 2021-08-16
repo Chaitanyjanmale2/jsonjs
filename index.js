45b7af9110e00538947f677799fd6d25f8656c03
chaitany <chaitanyjanmale@gmail.com>
2021-08-16 20:56:53 +0530

    updated css

--- index.js
+++ index.js
@@ -1,60 +1,30 @@
-/* ===>>>//FOR PRACTICE//<<<==== */
+ function createTable(tableData) {
+    var table = document.createElement('table');
+    var tableBody = document.createElement('tbody');
 
-/* var el_up = document.getElementById("GFG_UP");
-          
-var list = [
-    { "col_1": "val_11", "col_3": "val_13" },
-    { "col_2": "val_22", "col_3": "val_23" },
-    { "col_1": "val_31", "col_3": "val_33" }
-];
   
-el_up.innerHTML = "Click on the button to create "
-        +   "the table from the JSON data.<br><br>"
-        + JSON.stringify(list[0]) + "<br>" 
-        + JSON.stringify(list[1]) + "<br>"
-        + JSON.stringify(list[2]);   
-      
-function constructTable(selector) {
-      
-    // Getting the all column names
-    var cols = Headers(list, selector);  
+    tableData.forEach(function(rowData) {
+      var row = document.createElement('tr');//horizontal row start row=>
 
-    // Traversing the JSON data
-    for (var i = 0; i < list.length; i++) {
-        var row = $('<tr/>');   
-        for (var colIndex = 0; colIndex < cols.length; colIndex++)
-        {
-            var val = list[i][cols[colIndex]];
-              
-            // If there is any key, which is matching
-            // with the column name
-            if (val == null) val = "";  
-                row.append($('<td/>').html(val));
-        }
-          
-        // Adding each row to the table
-        $(selector).append(row);
-    }
-}
   
-function Headers(list, selector) {
-    var columns = [];
-    var header = $('<tr/>');
-      
-    for (var i = 0; i < list.length; i++) {
-        var row = list[i];
-          
-        for (var k in row) {
-            if ($.inArray(k, columns) == -1) {
-                columns.push(k);
-                  
-                // Creating the header
-                header.append($('<th/>').html(k));
-            }
-        }
-    }
-      
-    // Appending the header to the table
-    $(selector).append(header);
-        return columns;
-} */  
+      rowData.forEach(function(cellData) {
+        var cell = document.createElement('td');// row=>cell (building)
+            
+        cell.appendChild(document.createTextNode(cellData));//add text in cell 
+        row.appendChild(cell);//row =>cell=>TEXT<=cell end<=row end
+      });
+  
+      tableBody.appendChild(row);//row in table body table=>tablebody=>row =>cell=>TEXT<=cell end<=row end<=tablebody<=table
+    });
+  
+    table.appendChild(tableBody);//row added in table body
+   
+
+  } 
+
+    fetch("file.json").then(
+    responce=>responce.json()
+  ).then(data=> createTable(data)
+  
+  )
+ 
